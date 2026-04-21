f48dbe8a90b9f8748d0358e66c91ef1cef3da4cb
태영훈 <lifetae2021@gmail.com>
2026-04-21 10:21:30 +0900

    chore: force refresh v3.5.7 deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className="antialiased">
         <PublicLayout>{children}</PublicLayout>
         <div className="fixed bottom-4 right-4 z-[9999] pointer-events-none select-none text-black font-black text-lg bg-white/80 px-3 py-1 rounded-md shadow-sm">
-          v3.5.7
+          v3.5.7 {/* trigger_redeploy_20260421_1022 */}
         </div>
         <Toaster
           position="top-center"
