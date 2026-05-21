9fb9562c828874546544ee4cb0f82268311b7498
태영훈 <lifetae2021@gmail.com>
2026-05-21 16:23:35 +0900

    fix: include attended field in applications fromDoc mapper

--- src/lib/firestore/applications.ts
+++ src/lib/firestore/applications.ts
@@ -50,6 +50,7 @@ function fromDoc(snap: DocumentSnapshot): Application | null {
     avoidAcquaintance: d.avoidAcquaintance,
     etc: d.etc,
     slotNumber: d.slotNumber ?? undefined,
+    attended: d.attended ?? false,
     // 더미 계정 지원: users 컬렉션 없이 application에 직접 저장된 필드
     birthDate: d.birthDate,
     height: d.height,
